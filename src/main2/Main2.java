1e03b428532314050dbac58f7e7cae0aa7912554
luisreylarag <llara_ptc@upjr.edu.mx>
2021-02-17 09:50:16 -0600

    Primera version

---
+++ src/main2/Main2.java
@@ -0,0 +1,17 @@
+
+package main2;
+
+/**
+ *
+ * @author luisreylarag
+ */
+public class Main2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola UPJR");
+    }
+    
+}
