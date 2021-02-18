03596b5d67bdfc6ed18c8ca479b1207f2b1e4367
Luis Rey Lara Gonzalez <48841140+luisreylaragonzalez@users.noreply.github.com>
2021-02-18 14:26:40 -0600

    Add files via upload
    ejempos

---
+++ EjemploJueves18Feb_2.java
@@ -0,0 +1,21 @@
+
+package ejemplojueves18feb_2;
+
+public class EjemploJueves18Feb_2 {
+
+    public static void main(String[] args) {
+        int var=6; 
+        int var2 = 10;
+        boolean cond;
+        cond = true;
+        
+        if (!((var == 6) || (var2>5))){
+            System.out.println("Tu operacion es verdadera");
+        }else{
+            System.out.println("Tu operacion es falsa");
+
+        }
+       
+    }
+    
+}
---
+++ EjemploJueves18Feb_3.java
@@ -0,0 +1,16 @@
+
+package ejemplojueves18feb_3;
+
+
+public class EjemploJueves18Feb_3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for (int contador = 1; contador <=20;contador++){
+            contador= contador*2;
+            System.out.println("El contador vale: "+ contador);
+        }
+    }
+    
+}
